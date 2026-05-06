e49804c027065541bd58fbeb0ea3a68866b4cbd6
Santiago Padilla <padillasantiago51@gmail.com>
2026-05-06 12:30:44 -0300

    delete routes from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PropertyDetail } from "./shop/components/PropertyDetail";
 import HomePage from "./shop/pages/HomePage";
-import Login from "./views/auth/Login";
-import Register from "./views/auth/Resgister";
 import TasacionesForm from "./shop/pages/Tasaciones";
 
 function App() {
@@ -12,8 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/tasaciones" element={<TasacionesForm />} />
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/register" element={<Register />} />
           <Route path="/propiedad/:id" element={<PropertyDetail />} />
 
           <Route path="*" element={<h1>404 - No encontrado</h1>} />
