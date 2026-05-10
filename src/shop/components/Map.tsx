d458bc24e2c79a0c5f9cdb91889e47f08f76f396
Santiago Padilla <padillasantiago51@gmail.com>
2026-05-10 20:45:25 -0300

    fix icon marker

--- src/shop/components/Map.tsx
+++ src/shop/components/Map.tsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+const customIcon = new L.Icon({
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 function RecenterMap({ lat, lng }: { lat: number; lng: number }) {
   const map = useMap();
@@ -55,7 +66,7 @@ export const MapByAddress = ({ address }: { address: string }) => {
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[coords.lat, coords.lon]} />
+        <Marker position={[coords.lat, coords.lon]} icon={customIcon} />
         <RecenterMap lat={coords.lat} lng={coords.lon} />
       </MapContainer>
     </div>
